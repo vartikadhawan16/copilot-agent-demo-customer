935327f75a4505d5a62084166802079c24cd3a7b
Vartika Dhawan <vdhawan.v@gmail.com>
2025-11-19 17:31:23 +0000

    added code

---
+++ src/main/java/com/copilot/agent/customer/CopilotAgentDemoCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.copilot.agent.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotAgentDemoCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotAgentDemoCustomerApplication.class, args);
+	}
+
+}
